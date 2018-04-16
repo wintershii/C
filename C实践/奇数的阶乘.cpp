ba85d4d17168c30c96923a16a7242d5e1dea50dc
wintershii <wintershii@163.com>
2018-04-16 23:32:07 +0800

    new

--- C实践/奇数的阶乘.cpp
+++ C实践/奇数的阶乘.cpp
@@ -1,20 +1,24 @@
 #include<stdio.h>
 
 int newjc(int n){
-	int num;
-	if(n%2!=0)
-		num=n*newjc(n-1);
-	else num=1;
-	return num;
+	int num=1;
+	if(n>=1){
+	  if(n%2!=0)
+		 num=n*newjc(n-1);
+	  else num=newjc(n-1);
+    }
+	 return num;
 }
 
 int main(){
 	int a,r;
 	scanf("%d",&a);
 	while(a--){
-		int n;
+		int n;r=0;
 		scanf("%d",&n);
-		r=newjc(n);
+		for(int i=1;i<=n;i++){
+			r+=newjc(i);
+		}
 		printf("%d\n",r);
 	}
 	return 0;
